ff54fe95b4d8f5a5096a659d3a13830ae88c8a04
kaneth <you@example.com>
2023-10-15 17:04:02 -0500

    agregacion del rol user para invitados

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -1,6 +1,8 @@
-// exporta los nombres de los roles permitidos en el backend
-
-export const ROLE_ADMIN = "admin";
-export const ROLE_PROFESSOR = "professor";
-export const ROLE_STUDENT = "student";
-export const ROLE_USER = "user";
+/**
+ * exporta los nombres de los
+ * roles permitidos en el backend
+ */
+export const ROLE_ADMIN = process.env.REACT_APP_ROLE_ADMIN;
+export const ROLE_PROFESSOR = process.env.REACT_APP_ROLE_PROFESSOR;
+export const ROLE_STUDENT = process.env.REACT_APP_ROLE_STUDENT;
+export const ROLE_USER = process.env.REACT_APP_ROLE_USER;
