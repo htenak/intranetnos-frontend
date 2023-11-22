7bfe1c89a5201432528b88ed3cc2ac60ae40c1df
kaneth <kanethplayer@gmail.com>
2023-11-22 14:15:27 -0500

    pequeño cambio

--- src/components/pages/professor/other-classes/POtherClasses.js
+++ src/components/pages/professor/other-classes/POtherClasses.js
@@ -216,7 +216,7 @@ const POtherClasses = () => {
                               }
                             />
                           }
-                        ></Meta>
+                        />
                       </Card>
                     </CCol>
                   ))}
