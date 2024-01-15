7b7193c99884ef5d96f4f289e2504235b3dd7317
kaneth <kanethplayer@gmail.com>
2024-01-15 03:38:06 -0500

    nuevo archivo para constante de la ruta publica

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
+export * from "./publicURL";
 export * from "./roles";
--- src/constants/roles.js
+++ src/constants/roles.js
@@ -1,6 +1,8 @@
 /**
  * exporta los nombres de los
  * roles permitidos en el backend
+ *
+ * Se usan para validar el acceso a los menús y rutas, entre otros
  */
 export const ROLE_ADMIN = process.env.REACT_APP_ROLE_ADMIN;
 export const ROLE_PROFESSOR = process.env.REACT_APP_ROLE_PROFESSOR;
